3903e7f9db15a1b31d0d4200c1a4e2b4d906d5d8
Thea Whatling <K038218@eltham-college.org.uk>
2020-09-10 12:45:16 +0100

    Add project files.

---
+++ CreditCardCheck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CreditCardCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cardnum;
+            Console.WriteLine("please insert your credit card mnumber");
+            cardnum = Console.ReadLine();
+            Convert.ToInt32(cardnum);
+
+            //static bool luhn(string creditcardnum);
+
+        }
+    }
+}
